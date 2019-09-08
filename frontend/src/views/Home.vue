e2811e83fd2490754b49f8978a455ef10f7db9e0
minimac911 <thomasmcalpine77@gmail.com>
2019-09-08 20:08:58 +0200

    Setup Frontend and Backend

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  },
+  created() {
+    axios
+      .get("https://localhost:5001/api/values")
+      .then(response => {
+        // eslint-disable-next-line no-console
+        console.log(response);
+      })
+      .catch(e => {
+        this.errors.push(e);
+      });
+  }
+};
+</script>
